68a8d11444637aa86b935b39ccd437e7bda40183
Konstantina Gramatova <k.gramatova@prosyst.bg>
2020-02-06 22:27:22 +0200

    Major version changed

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/tinchouuu/libs
+module github.com/tinchouuu/libs/v2
 
 go 1.13
--- io/io.go
+++ io/io.go
@@ -2,6 +2,6 @@ package io
 
 import "fmt"
 
-func Write(text string) {
-	fmt.Println("from lib.io >>> " + text)
+func Write(text string, text2 string) {
+	fmt.Println("from lib.io >>> " + text+" text 2"+text2)
 }
